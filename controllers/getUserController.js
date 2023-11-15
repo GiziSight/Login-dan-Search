f415fef2af6b2854a1f4c97637a0b7e339fba974
Arrizal Bintang R <arrizalbramadhan09@gmail.com>
2023-11-15 20:25:18 +0700

    ganti logika usercontroller

--- controllers/getUserController.js
+++ controllers/getUserController.js
@@ -4,36 +4,24 @@ const conn = require('../dbConnection').promise();
 exports.getUser = async (req, res, next) => {
 
     try {
-
-        if (
-            !req.headers.authorization ||
-            !req.headers.authorization.startsWith('Bearer') ||
-            !req.headers.authorization.split(' ')[1]
-        ) {
-            return res.status(422).json({
-                message: "Please provide the token",
-            });
-        }
-
-        const theToken = req.headers.authorization.split(' ')[1];
-        const decoded = jwt.verify(theToken, 'the-super-strong-secrect');
-
+        const user = req.query
+        console.log(user)
         const [row] = await conn.execute(
-            "SELECT `id`,`username`,`email`,`gender`,`age`,`height`,`weight` FROM `users` WHERE `id`=?",
-            [decoded.id]
+            "SELECT id,username,email FROM users WHERE email=?",
+            [user.email]
         );
-
         if (row.length > 0) {
-            return res.json({
+            return res.status(200).json({
                 user: row[0]
             });
         }
-
-        res.json({
+        res.status(400).json({
             message: "No user found"
         });
 
-    } catch (err) {
+    }
+    catch (err) {
         next(err);
     }
+
 }
