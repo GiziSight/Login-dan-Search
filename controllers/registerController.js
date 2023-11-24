c49b1df16bbc33873c6735d6f7a7ceb39f842b26
fal06 <142571611+fal06@users.noreply.github.com>
2023-11-24 13:49:24 +0700

    Update registerController.js

--- controllers/registerController.js
+++ controllers/registerController.js
@@ -1,47 +1,48 @@
-const { validationResult } = require('express-validator');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
+const { validationResult } = require('express-validator');
 const conn = require('../dbConnection').promise();
 
 exports.register = async (req, res, next) => {
-  const errors = validationResult(req);
+    const errors = validationResult(req);
 
-  if (!errors.isEmpty()) {
-    return res.status(422).json({ errors: errors.array() });
-  }
-
-  try {
-    const [row] = await conn.execute(
-      'SELECT `email` FROM `users` WHERE `email`=?',
-      [req.body.email]
-    );
-
-    if (row.length > 0) {
-      return res.status(201).json({
-        message: 'The E-mail already in use',
-      });
+    if (!errors.isEmpty()) {
+        return res.status(422).json({ errors: errors.array() });
     }
 
-    const hashPass = await bcrypt.hash(req.body.password, 12);
+    try {
+        const [row] = await conn.execute(
+            'SELECT `email` FROM `users` WHERE `email`=?',
+            [req.body.email]
+        );
 
-    const [rows] = await conn.execute(
-      'INSERT INTO `users`(`username`,`email`,`password`,`gender`,`age`,`height`,`weight`) VALUES(?,?,?,?,?,?,?)',
-      [
-        req.body.username,
-        req.body.email,
-        hashPass,
-        req.body.gender, 
-        req.body.age, 
-        req.body.height, 
-        req.body.weight, 
-      ]
-    );
+        if (row.length > 0) {
+            return res.status(201).json({
+                message: 'The E-mail already in use',
+            });
+        }
 
-    if (rows.affectedRows === 1) {
-      const token = jwt.sign({ email: req.body.email }, 'your-secret-key', { expiresIn: '1h' });
-      return res.status(201).json({ message: 'The user has been successfully inserted.', token });
+        const hashPass = await bcrypt.hash(req.body.password, 12);
+
+        const [rows] = await conn.execute(
+            'INSERT INTO `users`(`username`,`email`,`password`,`gender`,`birthdate`,`height`,`weight`) VALUES(?,?,?,?,?,?,?)',
+            [
+                req.body.username,
+                req.body.email,
+                hashPass,
+                req.body.gender,
+                req.body.birthdate,
+                req.body.height,
+                req.body.weight,
+            ]
+        );
+
+        if (rows.affectedRows === 1) {
+            // Generate JWT token after successful registration
+            const token = jwt.sign({ email: req.body.email }, 'your-secret-key', { expiresIn: '1h' });
+            return res.status(201).json({ message: 'The user has been successfully registered.', token });
+        }
+    } catch (err) {
+        next(err);
     }
-  } catch (err) {
-    next(err);
-  }
 };
